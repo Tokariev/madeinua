bbbe39edbaffb2b6c97e4ed4636a57f0fef52b33
Mykola <mykola.tokariev@gmail.com>
2018-03-08 18:04:43 +0100

    Create top header.

--- include/block-header.php
+++ include/block-header.php
@@ -1,2 +1,21 @@
-<div id="block-header"></div>
+<div id="block-header">
+   
+    <ul id="header-top-menu">
+        <li>made.in.ua</li>     
+    </ul>
+
+    <form action="/action_page.php"> 
+      <input type="text" id="search" name="search" placeholder="Поиск товара..">
+      <input type="submit" value="Поиск">
+    </form>
+    
+    <div id="block-top-menu-right">
+        <ul>
+           <li>Продать</li>
+           <li>Регистрация</li>
+           <li>Войти</li>
+           <li>Корзина</li>
+       </ul>
+    </div>
+</div>
 
