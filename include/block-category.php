adc4d554a6dde514c9eb69c06bd4ed335018ab2d
Mykola <mykola.tokariev@gmail.com>
2018-03-08 20:57:22 +0100

    Create top header.

--- include/block-category.php
+++ include/block-category.php
@@ -1,2 +1,14 @@
-<div id="block-category"></div>
+<div id="block-category">
+    <ul>
+        <li><a href="#">Jewelry</a></li>
+        <li><a href="#">Clothing</a></li>
+        <li><a href="#">Товары для дома</a></li>
+        <li><a href="#">Wedding</a></li>
+        <li><a href="#">Toys</a></li>
+        <li><a href="#">Искусство</a></li>
+        <li><a href="#">Товары для рукоделия</a></li>
+        <li><a href="#">Винтаж</a></li>
+    </ul>
+
+</div>
 
--- index.php
+++ index.php
@@ -9,6 +9,7 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <link href="css/reset.css" rel="stylesheet" type="text/css">
         <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
         <title>made.in.ua</title>
     </head>
     <body>
@@ -19,11 +20,14 @@ and open the template in the editor.
                 include ("./include/block-category.php");
             ?>
             
-            <div id="block-slider"></div>
-            <div id="block-about"></div>
-            <div id="block-new-products"></div>
-            <div id="block-popular"></div>
-            <div id="block-img-category"></div>
+            <div id="block-max-width">
+                <div id="block-slider"></div>
+                <div id="block-about"></div>
+                <div id="block-new-products"></div>
+                <div id="block-popular"></div>
+                <div id="block-img-category"></div>   
+            </div>
+            
             
             <?php
                 include("./include/footer.php");
